52dbb8103e0b41071f63699d78836c0944664819
Jack Allaire <jra10542@gmail.com>
2024-07-25 12:53:27 -0500

    Added light gradient to luminous objects

--- src/tmp/Lighting.java
+++ src/tmp/Lighting.java
@@ -6,8 +6,20 @@ import java.awt.geom.Ellipse2D;
 
 public class Lighting {
     public static void render(Graphics2D g, Handler handler) {
-        Color darkness = new Color(0, 0, 0, 150);
-        Area darknessBounds = new Area(new Rectangle.Double(0, 0, Game.sWidth, Game.sHeight));
+        Color[] darkness = new Color[5];
+        darkness[0] = new Color(0, 0, 0, 0);
+        darkness[1] = new Color(0, 0, 0, 25);
+        darkness[2] = new Color(0, 0, 0, 50);
+        darkness[3] = new Color(0, 0, 0, 75);
+        darkness[4] = new Color(0, 0, 0, 100);
+
+        Area[] lightBounds = new Area[5];
+        lightBounds[0] = new Area(new Rectangle.Double(0, 0, Game.sWidth, Game.sHeight));
+        lightBounds[1] = new Area(new Rectangle.Double(0, 0, Game.sWidth, Game.sHeight));
+        lightBounds[2] = new Area(new Rectangle.Double(0, 0, Game.sWidth, Game.sHeight));
+        lightBounds[3] = new Area(new Rectangle.Double(0, 0, Game.sWidth, Game.sHeight));
+        lightBounds[4] = new Area(new Rectangle.Double(0, 0, Game.sWidth, Game.sHeight));
+
         float luminosity = 0;
         float targetX = 0;
         float targetY = 0;
@@ -15,6 +27,7 @@ public class Lighting {
         //Loop through all objects
         for(int i = 0; i < handler.object.size(); i++) {
             GameObject tempObject = handler.object.get(i);
+            Shape[] lightCircles = new Shape[5];
 
             //Apply light to objects with luminosity
             if (tempObject.getLuminosity() > 0) {
@@ -23,14 +36,22 @@ public class Lighting {
                 luminosity = tempObject.getLuminosity();
                 float variation = (float) Math.random();
 
-                Shape lightCircle = new Ellipse2D.Double(targetX - (luminosity/2), targetY - (luminosity/2) + variation, luminosity, luminosity + variation);
-                Area lightArea = new Area(lightCircle);
+                //Create gradient circles
+                for(int ii = 0; ii < 5; ii++) {
+                    double lightMod = ((ii + 1.0) / 5.0);
+                    Shape lightCircle = new Ellipse2D.Double((targetX - ((luminosity / 2) * lightMod)), (targetY - ((luminosity / 2) * lightMod)) + variation, luminosity * lightMod, (luminosity + variation) * lightMod);
+                    lightCircles[ii] = lightCircle;
 
-                darknessBounds.subtract(lightArea);
+                    Area lightArea = new Area(lightCircles[ii]);
+                    lightBounds[ii].subtract(lightArea);
+                }
             }
         }
 
-        g.setColor(darkness);
-        g.fill(darknessBounds);
+        //Apply darkness
+        for(int i = 4; i >= 0; i--) {
+            g.setColor(darkness[i]);
+            g.fill(lightBounds[i]);
+        }
     }
 }
